9871533ea47852d47c9f4611e2ec99314e318279
09eric09 <09eric09@gmail.com>
2022-07-18 22:21:29 -0700

    created AddUser.js component

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const AddUser = () => {
+
+    const addUserhandler = (e) => {
+        e.preventDefault();
+        
+    }
+
+  return (
+    <form onSubmit={addUserhandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
+        <label htmlFor="age">Age</label>
+        <input type="number" id="age" />
+        <button type="submit">Add User</button>
+    </form>
+  )
+}
+
+export default AddUser;
