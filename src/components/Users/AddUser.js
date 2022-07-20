3a5eb84cdacb2fd858575a0d5a4c881f060be14e
09eric09 <09eric09@gmail.com>
2022-07-19 21:16:20 -0700

    added basic form validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -18,16 +18,26 @@ const AddUser = () => {
 
     const addUserhandler = (e) => {
       e.preventDefault();
+      if (username.trim().length === 0 || age.trim().length === 0) {
+        return;
+      }
+
+      if (+age < 1) {
+        return;
+      }
+
       console.log(username, age);
+      setUsername('');
+      setAge('');
   }
 
   return (
     <Card className={classes.input}>
     <form onSubmit={addUserhandler}>
         <label htmlFor="username">Username</label>
-        <input onChange={userNameHandler} type="text" id="username" />
+        <input onChange={userNameHandler} value={username} type="text" id="username" />
         <label htmlFor="age">Age</label>
-        <input onChange={ageHandler} type="number" id="age" />
+        <input onChange={ageHandler} value={age} type="number" id="age" />
         <Button type="submit">Add User</Button>
     </form>
     </Card>
