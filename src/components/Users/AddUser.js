c1687c73972b71c9ed6894fe6d999bc97110b3c8
09eric09 <09eric09@gmail.com>
2022-07-19 21:07:53 -0700

    added state on submit

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
 
 const AddUser = () => {
 
-    const addUserhandler = (e) => {
-        e.preventDefault();
+  const [username, setUsername] = useState('');
+  const [age, setAge] = useState('');
 
+    const userNameHandler = (e) => {
+      setUsername(e.target.value);
     }
+
+    const ageHandler = (e) => {
+      setAge(e.target.value);
+    }
+
+    const addUserhandler = (e) => {
+      e.preventDefault();
+      console.log(username, age);
+  }
 
   return (
     <Card className={classes.input}>
     <form onSubmit={addUserhandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
+        <input onChange={userNameHandler} type="text" id="username" />
         <label htmlFor="age">Age</label>
-        <input type="number" id="age" />
+        <input onChange={ageHandler} type="number" id="age" />
         <Button type="submit">Add User</Button>
     </form>
     </Card>
